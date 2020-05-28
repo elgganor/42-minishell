74c252352d6e8e3ead26edf08a752157acf1945c
Astrid <astridelcros@hotmail.fr>
2020-05-28 15:21:21 +0200

    correction argc pwd

--- src/builtin/pwd.c
+++ src/builtin/pwd.c
@@ -6,7 +6,7 @@
 /*   By: astriddelcros <marvin@42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/24 16:20:38 by astriddel         #+#    #+#             */
-/*   Updated: 2020/05/28 14:52:42 by astriddel        ###   ########.fr       */
+/*   Updated: 2020/05/28 15:20:46 by astriddel        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@ int	builtin_pwd(char *command)
 {
 	char	*path;
 	char	buf[PATH_MAX + 1];
-	(void)command;
 	int	i;
 
-	i = 0;
-	if (command[i] > 0)
+	i = -1;
+	while (command[i])
+		i++;
+	if (i > 0)
 	{
 		ft_putstr("Wrong number of arguments");
 		return (0);
