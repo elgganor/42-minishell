6a18d5a04824d3a00ac4a07657336c07736c44a9
Mohamed-Younès <mohamed.rouabehi.pro@gmail.com>
2020-06-09 10:20:45 +0200

    Norme minishell.h

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <mrouabeh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/14 17:40:10 by mrouabeh          #+#    #+#             */
-/*   Updated: 2020/06/08 12:10:33 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2020/06/09 10:20:15 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,59 +41,59 @@ typedef struct	s_env
 ** ===== MINISHELL PROTOTYPES =====
 */
 
-t_env	*g_env;
-void	shell_loop();
+t_env			*g_env;
+void			shell_loop();
 
 /*
 ** EXECUTION PROTOTYPES
 */
 
-int		execute_commands(char **commands);
-int		run_builtin(char **command);
-int		run_system_program(char **command);
-int		is_executable(char *bin);
-void	get_absolute_path(char **command);
-char	*join_path(char *path, char *command);
+int				execute_commands(char **commands);
+int				run_builtin(char **command);
+int				run_system_program(char **command);
+int				is_executable(char *bin);
+void			get_absolute_path(char **command);
+char			*join_path(char *path, char *command);
 
 /*
 ** INITIALISATION PROTOTYPES
 */
 
-void	show_prompt(void);
-void	init_env(char **env);
-char	*get_env_var(char *var);
-void	append_env(char *var);
+void			show_prompt(void);
+void			init_env(char **env);
+char			*get_env_var(char *var);
+void			append_env(char *var);
 
 /*
 ** PARSING PROTOTYPES
 */
 
-char	*take_input(void);
-char	**list_commands(char *line);
-char	**split_command(char *command);
-int		is_builtin(char *command);
-int		redirection(char ***command);
-int		redirect_output(char **command, int pos, int type);
-int		redirect_input(char **command, int pos);
-void	clear_redirection(void);
-void	parse_env_var(char **command);
+char			*take_input(void);
+char			**list_commands(char *line);
+char			**split_command(char *command);
+int				is_builtin(char *command);
+int				redirection(char ***command);
+int				redirect_output(char **command, int pos, int type);
+int				redirect_input(char **command, int pos);
+void			clear_redirection(void);
+void			parse_env_var(char **command);
 
 /*
 ** UTILS PROTOTYPES
 */
 
-void	ft_puterr(char *err);
-void	free_env(void);
-void	free_split(char **str);
-int		clear_command_of_redirection(char ***command);
+void			ft_puterr(char *err);
+void			free_env(void);
+void			free_split(char **str);
+int				clear_command_of_redirection(char ***command);
 
 /*
 ** BUILT-INS PROTOTYPES
 */
 
-int		builtin_pwd(void);
-int		builtin_cd(char **command);
-int		builtin_env(void);
-int		builtin_exit(void);
+int				builtin_pwd(void);
+int				builtin_cd(char **command);
+int				builtin_env(void);
+int				builtin_exit(void);
 
 #endif
