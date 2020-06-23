6bb574ca59c6b41e5938644389241b4c2cfb4501
Astrid Delcros <astriddelcros@gmail.com>
2020-06-23 17:47:45 +0200

    echo x i

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: astriddelcros <marvin@42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/09 18:53:43 by astriddel         #+#    #+#             */
-/*   Updated: 2020/06/12 16:38:12 by astriddel        ###   ########.fr       */
+/*   Updated: 2020/06/23 17:46:31 by astriddel        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,13 @@ int     builtin_echo(char **command)
     i = 1;
     while (command[i] != NULL)
     {
-        write(1, command[i], ft_strlen(command[i]));
+        if (command[i + 1] == NULL)
+        {
+            ft_putstr(command[i]);
+            return (1);
+        }
+        ft_putstr(command[i]);
+        write(1, " ", 1);
         i++;
     }
     return (1);
