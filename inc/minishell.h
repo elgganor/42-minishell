1422cb8acf7feb9c1a6cddd1ad767c0ac57f88d0
mrouabeh <mohamed.rouabehi.pro@gmail.com>
2020-07-14 09:10:58 +0200

    Norme minishell.h

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <mrouabeh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/14 17:40:10 by mrouabeh          #+#    #+#             */
-/*   Updated: 2020/07/14 07:27:51 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2020/07/14 09:10:36 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,12 @@
 # include <limits.h>
 # include <signal.h>
 # include <sys/stat.h>
-# include <errno.h> 
+# include <errno.h>
 
 /*
+**----------------------------------------**
 ** STRUCTURES
+**----------------------------------------**
 */
 
 typedef struct	s_env
@@ -42,35 +44,41 @@ typedef struct	s_env
 	struct s_env	*next;
 }				t_env;
 
-typedef struct s_new
-{
-    char            *cmd;
-    struct s_new    *next;
-}               t_new;
-
 /*
-** ===== MINISHELL PROTOTYPES =====
+**----------------------------------------**
+** GLOBAL VARIABLES
+**----------------------------------------**
 */
 
 t_env			*g_env;
-t_new           *g_new;
+
+/*
+**----------------------------------------**
+** MINISHELL PROTOTYPES
+**----------------------------------------**
+*/
+
 void			shell_loop();
 
 /*
+**----------------------------------------**
 ** EXECUTION PROTOTYPES
+**----------------------------------------**
 */
 
-int		execute_commands(char *commands);
-int		process_commands(char **commands);
-int		execute_piped_commands(char **piped_commands);
-int		run_builtin(char **command);
-int		run_system_program(char **command);
-int		is_executable(char *bin);
-void	get_absolute_path(char **command);
-char	*join_path(char *path, char *command);
+int				execute_commands(char *commands);
+int				process_commands(char **commands);
+int				execute_piped_commands(char **piped_commands);
+int				run_builtin(char **command);
+int				run_system_program(char **command);
+int				is_executable(char *bin);
+void			get_absolute_path(char **command);
+char			*join_path(char *path, char *command);
 
 /*
+**----------------------------------------**
 ** INITIALISATION PROTOTYPES
+**----------------------------------------**
 */
 
 void			show_prompt(void);
@@ -79,22 +87,26 @@ char			*get_env_var(char *var);
 void			append_env(char *var);
 
 /*
+**----------------------------------------**
 ** PARSING PROTOTYPES
+**----------------------------------------**
 */
 
-char	*take_input(void);
-char	**split_input(char *input);
-char	**split_command(char *command);
-int		is_builtin(char *command);
-int		redirection(char ***command);
-int		redirect_output(char **command, int pos, int type);
-int		redirect_input(char **command, int pos);
-void	clear_redirection(void);
-void	parse_env_var(char **command);
-char	**parse_pipe(char *command);
+char			*take_input(void);
+char			**split_input(char *input);
+char			**split_command(char *command);
+int				is_builtin(char *command);
+int				redirection(char ***command);
+int				redirect_output(char **command, int pos, int type);
+int				redirect_input(char **command, int pos);
+void			clear_redirection(void);
+void			parse_env_var(char **command);
+char			**parse_pipe(char *command);
 
 /*
+**----------------------------------------**
 ** UTILS PROTOTYPES
+**----------------------------------------**
 */
 
 void			ft_puterr(char *err);
@@ -103,14 +115,16 @@ void			free_split(char **str);
 int				clear_command_of_redirection(char ***command);
 
 /*
+**----------------------------------------**
 ** BUILT-INS PROTOTYPES
+**----------------------------------------**
 */
 
 int				builtin_pwd(void);
 int				builtin_cd(char **command);
 int				builtin_env(void);
 int				builtin_exit(void);
-int       builtin_echo(char **command);
-int       builtin_export(char **command);
+int				builtin_echo(char **command);
+int				builtin_export(char **command);
 
 #endif
