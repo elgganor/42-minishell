89ef85c15a3c3e1edaac1c29621a96cf24ec3734
Astrid Delcros <astriddelcros@gmail.com>
2020-07-25 11:32:21 +0200

    cd% renvoie bad address

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: astriddelcros <marvin@42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/24 15:52:16 by astriddel         #+#    #+#             */
-/*   Updated: 2020/07/22 23:24:08 by astriddel        ###   ########.fr       */
+/*   Updated: 2020/07/25 11:31:23 by astriddel        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,15 @@ int     builtin_cd(char **command)
     int     new_dir;
     t_env   *directory;
     char    *oldpwd_value;
+    char    *home_path;
     oldpwd_value = getcwd(NULL, 0);
-	new_dir = chdir(command[1]);
     directory = g_env;
+    home_path = "";
+    if (ft_strcmp(directory->key, "HOME") == 0)
+        home_path = directory->value;
+    if (!command[1])
+        new_dir = chdir(home_path); // renvoie ERREUR : Bad address%
+    new_dir = chdir(command[1]);
     if (new_dir == -1)
     {
         printf("ERREUR : %s", strerror(errno));
