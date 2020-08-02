ca3600826eb7ceb8bf5bb0fdfb57cf5466d1fc71
Astrid Delcros <astriddelcros@gmail.com>
2020-08-02 23:52:27 +0200

    réparer cd seul

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: astriddelcros <marvin@42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/24 15:52:16 by astriddel         #+#    #+#             */
-/*   Updated: 2020/07/25 11:31:23 by astriddel        ###   ########.fr       */
+/*   Updated: 2020/08/02 23:50:37 by astriddel        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,22 @@ int     builtin_cd(char **command)
     int     new_dir;
     t_env   *directory;
     char    *oldpwd_value;
-    char    *home_path;
+    
     oldpwd_value = getcwd(NULL, 0);
     directory = g_env;
-    home_path = "";
-    if (ft_strcmp(directory->key, "HOME") == 0)
-        home_path = directory->value;
-    if (!command[1])
-        new_dir = chdir(home_path); // renvoie ERREUR : Bad address%
     new_dir = chdir(command[1]);
     if (new_dir == -1)
     {
         printf("ERREUR : %s", strerror(errno));
         return (0);
     }
+    else
+    {
+        if (!command[1] || ft_strcmp(command[1], "~"))
+            new_dir = chdir(get_env_var("HOME"));
+        else
+            new_dir = chdir(command[1]);
+    } 
     while (directory != NULL)
     {
         if (ft_strcmp(directory->key, "OLDPWD") == 0)
