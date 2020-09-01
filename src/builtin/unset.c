8ea9cd0c7a5aeeb4935372614ef9e2cdadc88d04
Astrid Delcros <astriddelcros@gmail.com>
2020-09-01 10:54:51 +0200

    unset ok

--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -6,37 +6,56 @@
 /*   By: astriddelcros <marvin@42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/22 23:44:45 by astriddel         #+#    #+#             */
-/*   Updated: 2020/08/22 17:16:53 by astriddel        ###   ########.fr       */
+/*   Updated: 2020/09/01 01:02:08 by astriddel        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "minishell.h"
 #include <stdio.h>
 
-int     builtin_unset(char **command)
+static int     exec_unset(char *key)
 {
-    t_env   *env;
+    t_env *env;
     t_env *to_rm;
-
     env = g_env;
-    if (!valid_key(command[1]))
+    if (ft_strcmp(env->key, key) == 0)
     {
-        ft_putstr("unset: ");
-        ft_putstr(command[1]);
-        ft_putstr(": not a valid identifier\n");
+        g_env = g_env->next;
+        free(env->key);
+        free(env->value);
+        free(env);
         return (1);
     }
-    while (env != NULL)
-    {
-        if (ft_strcmp(command[1], env->next->key) == 0)
-        {
-            to_rm = env->next;
-            env->next = to_rm->next;
+    while (env->next) {
+        if (ft_strcmp(env->next->key, key) == 0)
+		{
+			to_rm = env->next;
+			env->next = to_rm->next;
             free(to_rm->key);
             free(to_rm->value);
             free(to_rm);
+		    return (1);
+		}
+		else
+			env = env->next;
+    }
+    return (1);
+}
+
+int     builtin_unset(char **command)
+{
+    int i;
+    i = 0;
+    while (command[++i])
+    {
+        if (valid_key(command[i]))
+            exec_unset(command[i]);
+        else
+        {
+            ft_putstr("unset: ");
+            ft_putstr(command[i]);
+            ft_putstr(": not a valid identifier\n");
+            continue;
         }
-        env = env->next;
     }
     return (1);
 }
